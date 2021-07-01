e0adbe8f48d40f81747b4521cd3100eef19be554
Stoyan Valkov <m0673n@abv.bg>
2021-07-01 10:27:40 +0300

    Fixed bug.

--- 02_classes_and_instances/exercise/07_problem/project/library.py
+++ 02_classes_and_instances/exercise/07_problem/project/library.py
@@ -14,8 +14,8 @@ class Library:
             return "We could not find such user to remove!"
         self.user_records.remove(user)
         for book in user.books:
-            if book in self.rented_books[user]:
-                self.rented_books.pop(book)
+            if book in self.rented_books[user.username]:
+                self.rented_books[user.username].pop(book)
 
     def change_username(self, user_id, new_username):
         for user in self.user_records:
@@ -25,7 +25,8 @@ class Library:
                            " it should be different than the username used so far!"
                 else:
                     if user.username in self.rented_books:
-                        self.user_records[user.username] = new_username
+                        self.rented_books[new_username] = self.rented_books[user.username]
+                        self.rented_books.pop(user.username)
                     user.username = new_username
                     return f"Username successfully changed to: {new_username} for userid: {user_id}"
         else:
