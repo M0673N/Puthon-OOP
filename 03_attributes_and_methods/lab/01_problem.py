a1c2d2950aa83d3294853f5d670835e8710c44f2
Stoyan Valkov <m0673n@abv.bg>
2021-07-15 20:26:41 +0300

    They changed the problem a bit.

--- 03_attributes_and_methods/lab/01_problem.py
+++ 03_attributes_and_methods/lab/01_problem.py
@@ -1,4 +1,4 @@
-class Store:
+class Shop:
     def __init__(self, name, type_, capacity):
         self.name = name
         self.type = type_
@@ -9,31 +9,22 @@ class Store:
         return f"{self.name} of type {self.type} with capacity {self.capacity}"
 
     @classmethod
-    def from_size(cls, name, type_, size):
-        return cls(name, type_, size / 2)
+    def small_shop(cls, name, type_):
+        return cls(name, type_, 10)
+
+    def check_capacity(self):
+        return sum([self.items[items] for items in self.items])
 
     def add_item(self, item_name):
-        if sum(value for value in self.items.values()) >= self.capacity:
-            return "Not enough capacity in the store"
+        if self.check_capacity() >= self.capacity:
+            return "Not enough capacity in the shop"
         if item_name not in self.items:
             self.items[item_name] = 0
         self.items[item_name] += 1
-        return f"{item_name} added to the store"
+        return f"{item_name} added to the shop"
 
     def remove_item(self, item_name, amount):
         if item_name not in self.items or self.items[item_name] < amount:
             return f"Cannot remove {amount} {item_name}"
         self.items[item_name] -= amount
-        return f"{amount} {item_name} removed from the store"
-
-
-# first_store = Store("First store", "Fruit and Veg", 20)
-# second_store = Store.from_size("Second store", "Clothes", 500)
-#
-# print(first_store)
-# print(second_store)
-#
-# print(first_store.add_item("potato"))
-# print(second_store.add_item("jeans"))
-# print(first_store.remove_item("tomatoes", 1))
-# print(second_store.remove_item("jeans", 1))
+        return f"{amount} {item_name} removed from the shop"
