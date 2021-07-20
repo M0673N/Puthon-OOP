0b211497313ef3bc74945c534003a73a3bea6648
Stoyan Valkov <m0673n@abv.bg>
2021-07-20 12:48:56 +0300

    Misread description and had to change the solution a bit.

--- 09_decorators/exercise/08_problem.py
+++ 09_decorators/exercise/08_problem.py
@@ -1,12 +1,13 @@
-def store_results(func):
-    def wrapper(*args, **kwargs):
-        result = func(*args, **kwargs)
-        to_be_logged = f"Function '{func.__name__}' was called. Result: {result}\n"
+class store_results:
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        result = self.func(*args, **kwargs)
+        to_be_logged = f"Function '{self.func.__name__}' was called. Result: {result}\n"
         with open("results.txt", "a") as file:
             file.write(to_be_logged)
         return result
-
-    return wrapper
 
 
 @store_results
